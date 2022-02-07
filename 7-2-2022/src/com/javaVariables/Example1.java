44b75824f55c6b985d5f382b9a2c3a977e9b1953
Manish Yadav <Manish Yadav@LAPTOP-ENE1TNDU>
2022-02-07 18:50:25 +0530

    com.javaVariables 7-2-2022

---
+++ 7-2-2022/src/com/javaVariables/Example1.java
@@ -0,0 +1,35 @@
+package com.javaVariables;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		// Write a java program to print student information using java variables and initialization
+		
+		//Variable declaration
+		int studentId;
+		String studentName;
+		float studentMarks;
+		String studentAddress;
+		
+		//Variable intialization
+		studentId = 3020;
+		studentName ="Samyuktha";
+		studentMarks = 58.45f;
+		studentAddress = "Hyderabad";
+		
+		System.out.print("student Id: ");
+		System.out.println("studentId");
+		
+		System.out.print("student Name: ");
+		System.out.println("studentName");
+		
+		System.out.print("student Marks: ");
+		System.out.println("studentMarks");
+		
+		System.out.print("student Address: ");
+		System.out.println("studentAddress");
+
+	}
+
+}
